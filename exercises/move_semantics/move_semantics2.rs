d5150bafe645eee9c4b142ea466c4e5fe74b2de2
Ryan <ryan.p.kav@gmail.com>
2021-09-28 23:20:51 +0100

    First round of exercises

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,7 +7,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -18,7 +18,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,11 +7,11 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
-// Put your function here!
-// fn ..... {
 
+fn calculate_apple_price(apples: i32) -> i32 {
+	if apples > 40 { apples } else { 2 * apples } 
+} 
 // Don't modify this function!
 #[test]
 fn verify_test() {
